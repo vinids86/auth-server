1f5807972037275dc2e751c1cc6181b13fb20baa
Vinicius.saraiva Acc <vinicius.saraiva.acc@accurate.com.br>
2018-01-12 19:37:37 -0200

    Dockerize application

--- src/main/java/br/com/makeshiftonjava/authserver/AuthServerApplication.java
+++ src/main/java/br/com/makeshiftonjava/authserver/AuthServerApplication.java
@@ -2,10 +2,10 @@ package br.com.makeshiftonjava.authserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
-import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class AuthServerApplication {
 
 	public static void main(String[] args) {
